b50cb704b3a41a7b2d222965d1e5d4a0e17e9124
AmishaMe24 <amishamehta24@gmail.com>
2020-03-11 16:30:43 +0530

    Fragments Added

--- app/src/main/java/com/example/risingindians/HomeActivity.java
+++ app/src/main/java/com/example/risingindians/HomeActivity.java
@@ -3,13 +3,17 @@ package com.example.risingindians;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +30,47 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+
+            case R.id.navigation_home:
+                fragment = new EventsFragment();
+                break;
+
+            case R.id.navigation_donate:
+                fragment = new DonateFragment();
+                break;
+
+            case R.id.navigation_timeline:
+                fragment = new TimelineFragment();
+                break;
+
+            case R.id.navigation_account:
+                fragment = new AccountFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
     }
 }
